82c3926de2654c85585c8986cd562bdc725df095
wisnu318 <92870248+wisnu318@users.noreply.github.com>
2023-11-05 16:16:28 +0700

    revisi 3

--- tugas1.js
+++ tugas1.js
@@ -15,19 +15,21 @@ const cekHariKerja = (day) => {
 }
 
 // // buatkan sambungan dengan then catch untuk cek hari kerja dari kode async dan jelaskan penggunaan then catch dengan komentar, jika sebuah fungsi mereturn sebuah promise bisa langsung chainning then
-// cekHariKerja('senin').then((result)=>console.log(`hari ${result} adalah hari kerja`)).catch((err)=>console.log(err.message))
+cekHariKerja('senin').
+then((result)=>console.log(`hari ${result} adalah hari kerja`)).
+catch((err)=>console.log(err.message))
 // cekHariKerja('minggu').then((result)=>console.log(`hari ${result} adalah hari kerja`)).catch((err)=>console.log(err.message))
 
-// buatkan sambungan dengan try catch untuk cek hari kerja dari kode async dan jelaskan penggunaan try catch dengan komentar
-const hariKerja = async () => {
- try {
-    const hari = await cekHariKerja('kamis')
-    console.log(`hari ${hari} adalah hari kerja`)
+// // buatkan sambungan dengan try catch untuk cek hari kerja dari kode async dan jelaskan penggunaan try catch dengan komentar
+// const hariKerja = async () => {
+//  try {
+//     const hari = await cekHariKerja('kamis')
+//     console.log(`hari ${hari} adalah hari kerja`)
 
-    // const hari = await cekHariKerja('minggu')
-    // console.log(`hari ${hari} adalah hari kerja`)
- }catch (err) {
-    console.log (err.message);
- }
-};
-hariKerja();
+//     // const hari = await cekHariKerja('minggu')
+//     // console.log(`hari ${hari} adalah hari kerja`)
+//  }catch (err) {
+//     console.log (err.message);
+//  }
+// };
+// hariKerja();
--- tugas3.js
+++ tugas3.js
@@ -11,6 +11,7 @@
 //     })
 // }
 
+// //ambil data
 // getData(dataInput => {
 //     console.log(dataInput.map(item => item.name))
 // })
@@ -27,22 +28,22 @@ const getData = async () => {
     }
 }
 
-// ambil data pakai then catch
-getData().then(result => {
-    const data = result
-    console.log(data.map(item => item.name))
-}).catch(error => {
-    console.log(error.message)
-})
+// // ambil data pakai then catch
+// getData().then(result => {
+//     const data = result
+//     console.log(data.map(item => item.name))
+// }).catch(error => {
+//     console.log(error.message)
+// })
 
-// //ambil data nama pakai async await
-// const dataName = async () => {
-//     try{
-//         const result = await getData()
-//         console.log(result.map(item => item.name))
-//     }catch (err) {
-//         console.log (err.message);
-//      }
-// }
+//ambil data nama pakai async await
+const dataName = async () => {
+    try{
+        const result = await getData()
+        console.log(result.map(item => item.name))
+    }catch (err) {
+        console.log (err.message);
+     }
+}
 
-// dataName();
+dataName(getData);
